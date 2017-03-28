b2d49b0459bf8d6e51ccef42db8c824f5ea36c3a
Tommy Ho <tommy8694@gmail.com>
2017-03-28 23:51:38 +0800

    fix delegation action api

--- app/scripts/collections/delegationAction.js
+++ app/scripts/collections/delegationAction.js
@@ -1,13 +1,13 @@
-/*global Hktdc, Backbone*/
+/* global Hktdc, Backbone */
 
 Hktdc.Collections = Hktdc.Collections || {};
 
-(function () {
+(function() {
   'use strict';
 
   Hktdc.Collections.DelegationAction = Backbone.Collection.extend({
     url: function(type) {
-      return Hktdc.Config.apiURL + '/users/delegation-sharing-list/action?type=' + type;
+      return Hktdc.Config.apiURL + '/delegation-sharing-list/action?type=' + type;
     },
 
     model: Hktdc.Models.DelegationAction
