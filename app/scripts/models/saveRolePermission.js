e9d947e0ce78609ed952d7b6287721af68abf4f1
Tommy Ho <tommy8694@gmail.com>
2017-04-05 18:55:03 +0800

    role permission validation

--- app/scripts/models/saveRolePermission.js
+++ app/scripts/models/saveRolePermission.js
@@ -1,4 +1,4 @@
-/*global Hktdc, Backbone*/
+/* global Hktdc, Backbone, _ */
 
 Hktdc.Models = Hktdc.Models || {};
 
@@ -16,16 +16,37 @@ Hktdc.Models = Hktdc.Models || {};
     initialize: function() {},
 
     // defaults: {
-    //   RolePermissionGUID: '',
-    //   MenuItemGUID: '',
-    //   UserRoleGUID: ''
-    // },
-    
-    validate: function(attrs, options) {},
+    //   data: [
+    //     {
+    //       RolePermissionGUID: '',
+    //       MenuItemGUID: '',
+    //       UserRoleGUID: ''
+    //     }, ...
+    //   ]
+    // }
+
+    validate: function(attrs, options) {
+      var errors = [];
+      if (attrs.data && attrs.data.length === 0) {
+        errors.push('Role is required');
+      }
+      _.each(attrs.data, function(data) {
+        if (!data.MenuItemGUID) {
+          errors.push('Permission is required.');
+        }
+        if (!attrs.UserRoleGUID) {
+          errors.push('Role is required.');
+        }
+      });
+
+      if (errors.length) {
+        return _.uniq(errors).join('<br>');
+      }
+      return false;
+    },
 
     parse: function(response, options) {
       return response;
     }
   });
-
 })();
