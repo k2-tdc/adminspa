a3edd8d62e3c3a0ea89dd49849a41f13a82c5d52
Tommy Ho <tommy8694@gmail.com>
2017-05-04 11:21:35 +0800

    email profile validation

--- app/scripts/models/emailProfile.js
+++ app/scripts/models/emailProfile.js
@@ -1,4 +1,4 @@
-/*global Hktdc, Backbone*/
+/* global Hktdc, Backbone */
 
 Hktdc.Models = Hktdc.Models || {};
 
@@ -11,11 +11,27 @@ Hktdc.Models = Hktdc.Models || {};
       return Hktdc.Config.apiURL + '/users/' + Hktdc.Config.userID + '/email-profiles/' + profileId;
     },
 
-    initialize: function() {},
+    initialize: function() {
+      var self = this;
+      this.isInvalid = {
+        ProcessID: function() {
+          return (self.attributes.ProcessID && String(self.attributes.ProcessID) !== '0') ? false : 'Process is required.';
+        },
+        StepID: function() {
+          return (self.attributes.StepID && String(self.attributes.StepID) !== '0') ? false : 'Notification is required.';
+        },
+        DayOfWeek: function() {
+          return (self.attributes.DayOfWeek && self.attributes.DayOfWeek.length) ? false : 'Day of Week is required';
+        },
+        UserId: function() {
+          return (self.attributes.UserId && String(self.attributes.UserId) !== '0') ? false : 'User is required.';
+        }
+      };
+    },
 
     defaults: {
       ProfileId: '',
-      ProcessId: 0,
+      ProcessID: 0,
       ProcessName: 0,
       StepID: 0,
       TimeSlot: 1,
@@ -35,11 +51,38 @@ Hktdc.Models = Hktdc.Models || {};
       profileUserCollection: null
     },
 
-    validate: function(attrs, options) {},
+    validate: function(attrs, options) {
+      // *** valid => return false;
+      // *** invalid => return true;
+
+      // for single validate
+      if (options.field && !_.isArray(options.field)) {
+        if (this.isInvalid[options.field] && this.isInvalid[options.field]()) {
+          console.log('invalid: ', options.field, '>> ', this.isInvalid[options.field]());
+          options.onInvalid({
+            message: this.isInvalid[options.field]()
+          });
+          return true;
+        } else {
+          this.trigger('valid', {
+            field: options.field
+          });
+          return false;
+        }
+
+        // validate the whole form
+      } else {
+        return !(
+          !this.isInvalid.UserId() &&
+          !this.isInvalid.ProcessID() &&
+          !this.isInvalid.StepID() &&
+          !this.isInvalid.DayOfWeek()
+        );
+      }
+    },
 
     parse: function(response, options) {
       return response;
     }
   });
-
 })();
