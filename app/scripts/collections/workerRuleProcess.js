6c5a868d5ae2756c4b2d7a4babeac0eed137675a
Tommy Ho <tommy8694@gmail.com>
2017-05-12 19:59:10 +0800

    change worker rule process list api link

--- app/scripts/collections/workerRuleProcess.js
+++ app/scripts/collections/workerRuleProcess.js
@@ -8,7 +8,7 @@ Hktdc.Collections = Hktdc.Collections || {};
   Hktdc.Collections.WorkerRuleProcess = Backbone.Collection.extend({
 
     url: function(menuId) {
-      return Hktdc.Config.apiURL + '/worker-rules/process?MenuId=' + menuId;
+      return Hktdc.Config.apiURL + '/users/' + Hktdc.Config.userID + '/applications?page=' + menuId;
     },
 
     model: Hktdc.Models.Process
